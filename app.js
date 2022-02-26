60bc3179b2666e808bbc01a5998b40c439e7bbaa
Nithin-anim <nithin@animaker.com>
2022-02-26 23:00:40 +0530

    Add animations and effects

--- app.js
+++ app.js
@@ -1,11 +1,68 @@
+const words = [
+    'ADIEU',
+    'ACUTE',
+    'ARISE',
+    'AUDIO',
+    'AHEAD',
+    'ALLOW',
+    'ARGUE',
+    'BREAK',
+    'BRIEF',
+    'BAKER',
+    'COLOR',
+    'COUNT',
+    'CLAIM',
+    'DELAY',
+    'DOING',
+    'DREAM',
+    'DRESS',
+    'ELDER',
+    'EIGHT',
+    'EQUAL',
+    'EXACT',
+    'FAITH',
+    'FINAL',
+    'FRAME',
+    'FIELD',
+    'GRADE',
+    'GRANT',
+    'GLOBE',
+    'HUMOR',
+    'HEART',
+    'HOUSE',
+    'IDEAS',
+    'INDEX',
+    'INPUT',
+    'JUDGE',
+    'LIVES',
+    'MAGIC',
+    'MAJOR',
+    'NOISE',
+    'ORDER',
+    'PIECE',
+    'QUITE',
+    'REACT',
+    'SOARE',
+    'SOUND',
+    'TEAMS',
+    'THOSE',
+    'WATER',
+    'WASTE',
+    'YOUNG'
+];
+
 const state = {
     rowIndex: 0,
     columnIndex: 0,
     guesses: [[], [], [], [], [], []],
-    targetWord: 'SKILL'
+    targetWord: words[Math.floor(Math.random() * words.length)],
+    streak: 0,
+    plays: 0,
+    canType: true
 };
 
 const addKeyListeners = () => {
+    console.log(state.targetWord);
     const arr = Array.from(document.getElementsByClassName('key'))
     for (let index = 0; index < arr.length; index++) {
         arr[index].addEventListener('click', (e) => {
@@ -19,16 +76,18 @@ window.addEventListener('keydown', (e) => {
 });
 
 const inputGuess = (key) => {
+    if (!state.canType) {
+        return;
+    }
     if (isAlphabet(key)) {
         if (state.guesses[state.rowIndex].length !== 5) {
             state.guesses[state.rowIndex].push(key.toUpperCase());
             document.getElementById(`tile_${state.rowIndex}${state.columnIndex}`).textContent = key.toUpperCase();
+            document.getElementById(`tile_${state.rowIndex}${state.columnIndex}`).classList.add('shake-tile');
             state.columnIndex += 1;
         }
     } else if (key.toLowerCase() === 'enter' && state.guesses[state.rowIndex].length === 5) {
         compareGuess();
-        state.rowIndex += 1;
-        state.columnIndex = 0;
     } else if (key.toLowerCase() === 'backspace' && state.columnIndex !== 0) {
         state.columnIndex -= 1;
         state.guesses[state.rowIndex].pop()
@@ -39,29 +98,56 @@ const inputGuess = (key) => {
 
 const compareGuess = () => {
     let correctCount = 0;
-
+    state.canType = false;
     const { guesses, targetWord, rowIndex } = state;
+
     guesses[rowIndex].forEach((letter, index) => {
+        const tile = document.getElementById(`tile_${rowIndex}${index}`);
+        tile.classList.remove('shake-tile');
+        const key = document.getElementById(`key_${letter}`);
         if (letter === targetWord.charAt(index)) {
-            document.getElementById(`tile_${rowIndex}${index}`).style.backgroundColor = '#538D4E';
-            document.getElementById(`key_${letter}`).style.backgroundColor = '#538D4E';
+            setTimeout(() => {
+                tile.classList.add('rotate-tile');
+                tile.style.backgroundColor = '#538D4E';
+                key.style.backgroundColor = '#538D4E';
+            }, 500 * index);
             correctCount += 1;
         } else if (targetWord.indexOf(letter) > -1) {
-            document.getElementById(`tile_${rowIndex}${index}`).style.backgroundColor = '#D8D508';
-            document.getElementById(`key_${letter}`).style.backgroundColor = '#D8D508';
+            setTimeout(() => {
+                tile.classList.add('rotate-tile');
+                tile.style.backgroundColor = '#D8D508';
+                key.style.backgroundColor = '#D8D508';
+            }, 500 * index);
         } else {
-            document.getElementById(`tile_${rowIndex}${index}`).style.backgroundColor = '#BE3535';
-            document.getElementById(`key_${letter}`).style.backgroundColor = '#BE3535';
+            setTimeout(() => {
+                tile.classList.add('rotate-tile');
+                tile.style.backgroundColor = '#BE3535';
+                key.style.backgroundColor = '#BE3535';
+            }, 500 * index);
         }
     });
-    if (correctCount === 5) {
-        document.getElementsByClassName('overlay')[0].style.display = 'block';
-        document.getElementById('modal-title').textContent = 'YOU WIN 😄';
-    }
-    if (correctCount !== 5 && state.rowIndex === 5) {
-        document.getElementById('modal-title').textContent = 'YOU LOSE 😔';
-        document.getElementsByClassName('overlay')[0].style.display = 'block';
-    }
+    setTimeout(() => {
+        if (correctCount === 5) {
+            document.getElementById('modal-title').textContent = 'YOU WIN 😄';
+            document.getElementById('target-word').textContent = state.targetWord;
+            document.getElementById('streak-count').textContent = `${state.streak}`;
+            document.getElementById('plays-count').textContent = `${state.plays}`;
+            document.getElementsByClassName('overlay')[0].style.display = 'block';
+
+            state.canType = false;
+        } else if (correctCount !== 5 && state.rowIndex === 5) {
+            document.getElementById('modal-title').textContent = 'YOU LOSE 😔';
+            document.getElementById('target-word').textContent = state.targetWord;
+            document.getElementById('streak-count').textContent = `${state.streak}`;
+            document.getElementById('plays-count').textContent = `${state.plays}`;
+            document.getElementsByClassName('overlay')[0].style.display = 'block';
+            state.canType = false;
+        } else {
+            state.canType = true;
+        }
+        state.rowIndex += 1;
+        state.columnIndex = 0;
+    }, 2100);
 }
 
 const isAlphabet = (key) => {
